833fe20d3e5874f0f32a5470c09f00711d6d7234
Hiromi Takahashi <hiromix222@gmail.com>
2019-10-26 12:36:04 +0900

    modify for java11

--- src/main/java/com/example/omix222/demo22/Demo22Application.java
+++ src/main/java/com/example/omix222/demo22/Demo22Application.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Demo22Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo22Application.class, args);
 	}
-
+	
 }
