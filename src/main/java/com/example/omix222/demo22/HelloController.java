3e7f13c9f54c263c9a2e1db3d3793fc854dca315
Hiromi Takahashi <hiromix222@gmail.com>
2020-08-22 19:54:29 +0900

    add sample

--- src/main/java/com/example/omix222/demo22/HelloController.java
+++ src/main/java/com/example/omix222/demo22/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.omix222.demo22;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,59 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
     @RequestMapping("/")
-    public String hello(){
-        String result = helloService.concat("helllo ","SpringBoot 2.2");
-        return result;
+    public ToDto hello(){
+        FromDto from = new FromDto();
+        from.setHoge("hoge");
+        from.setHuga("huga");
+//        ToDto to = new ToDto();
+//        BeanUtils.copyProperties(from,to);
+        ToDto to = new ToDto(from.huga, from.hoge);
+        return to;
+    }
+
+    class FromDto {
+        private String huga;
+        private String hoge;
+        public void setHuga(String huga) {
+            this.huga = huga;
+        }
+        public String getHoge() {
+            return hoge;
+        }
+        public void setHoge(String hoge) {
+            this.hoge = hoge;
+        }
+        public String getHuga(){
+            return huga;
+        }
+    }
+    static class ToDto {
+        public ToDto(String huga, String hoge) {
+            this.huga = huga;
+            this.hoge = hoge;
+        }
+
+        public ToDto() {
+        }
+        public static ToDto generte(String huga,String hoge) {
+            return new ToDto(huga,
+                    hoge);
+        }
+        private String huga;
+        private String hoge;
+        public String getHuga() {
+            return huga;
+        }
+        public String getHoge() {
+            return hoge;
+        }
+
+        public void setHoge(String hoge) {
+            this.hoge = hoge;
+        }
+
+        public void setHuga(String huga) {
+            this.huga = huga;
+        }
     }
 }
